e4ab94ea596a51f7f84743a0881d45b783d1c4dd
Alfred Choi <alfred.choi@telus.com>
2017-07-04 17:16:07 -0400

    initial commit

--- HtmlTesting/ViewController.swift
+++ HtmlTesting/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let html = "<ul><li>testing</li></ul>"
+        if let data = html.data(using: .unicode) {
+            let paraStyle = NSMutableParagraphStyle()
+            let options: [String: Any] = [
+                NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                NSParagraphStyleAttributeName: paraStyle
+            ]
+            do {
+                let attributedString = try NSAttributedString(data: data, options: options, documentAttributes: nil)
+                textView.attributedText = attributedString
+            } catch {}
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
